2d48a4d416b3da8790ccfe2eb9f4d13b113d3108
RobotSigmund <ss@robotnorge.no>
2024-12-02 21:18:41 +0100

    Update server.pl

--- server.pl
+++ server.pl
@@ -16,11 +16,11 @@ print '---'."\n\n";
 # CONFIG
 
 # Listen on this IP and PORT. Use 0.0.0.0 to listen on all network adapters.
-my($SERVER_IP) = '0.0.0.0';
-my($SERVER_PORT) = '1337';
+our $SERVER_IP = '0.0.0.0';
+our $SERVER_PORT = '1337';
 
 # Example request=>responses
-my(%SERVER_RESPONSE) = (
+our %SERVER_RESPONSE = (
 	'1000'=>'1010',
 	'2000'=>'2010',
 	'3000'=>'3010');
@@ -30,41 +30,41 @@ my(%SERVER_RESPONSE) = (
 
 
 # creating a listening socket
-my($SERVER_SOCKET) = new IO::Socket::INET (
+my $server_socket = new IO::Socket::INET (
     LocalHost => $SERVER_IP,
     LocalPort => $SERVER_PORT,
     Proto => 'tcp',
     Listen => 5,
     Reuse => 1);
-die 'Cannot create socket '.$!."\n" unless $SERVER_SOCKET;
+die 'Cannot create socket '.$!."\n" unless $server_socket;
 
-my($CLIENT_PORT,$CLIENT_IP,$CLIENT_SOCKET,$client_data);
-my($proxy_request,$proxy_request_url,$proxy_request_auth);
+my($client_socket, $client_data);
 
 while (1) {
 	
 	print 'Waiting for new connection on port '.$SERVER_PORT.'...'."\n";
-	$client_data = ServerReceive();
-	ServerRespond(ResponseGen($client_data));
+	$client_data = ServerReceive(\$server_socket, \$client_socket);
+	ServerRespond(ResponseGen($client_data), \$client_socket);
 	print '---'."\n\n";
 	
 }
 
-$SERVER_SOCKET->close();
+$server_socket->close();
 
 exit;
 
 
 
 sub ServerReceive {
-    $CLIENT_SOCKET = $SERVER_SOCKET->accept();
-    $CLIENT_IP = $CLIENT_SOCKET->peerhost();
-    $CLIENT_PORT = $CLIENT_SOCKET->peerport();
-    print '  Connection from client '.$CLIENT_IP.':'.$CLIENT_PORT."\n";
+	my($server_socket_ref, $client_socket_ref) = @_;
+	
+    $$client_socket_ref = $$server_socket_ref->accept();
+    my $client_ip = $$client_socket_ref->peerhost();
+    my $client_port = $$client_socket_ref->peerport();
+    print '  Connection from client '.$client_ip.':'.$client_port."\n";
 
     # read up to 4096 characters from the connected client
-	my($client_data) = '';
-    $CLIENT_SOCKET->recv($client_data, 4096);
+    $$client_socket_ref->recv(my $client_data, 4096);
 	
 	# Remove trailing junk, eg. CR or LF
 	$client_data =~ s/[\r\n]$//g;
@@ -73,28 +73,30 @@ sub ServerReceive {
 	print FILE "\n".'////////// RECEIVED FROM CLIENT //////////'."\n";
 	print FILE $client_data;
 	close(FILE);
-    print '    received data from '.$CLIENT_IP.':'.$CLIENT_PORT."\n";
+    print '    received data from '.$client_ip.':'.$client_port.' ['.$client_data.']'."\n";
 	
-	return($client_data);
+	return $client_data;
 }
 
 
 
 sub ServerRespond {
-	my($rdata) = @_;
+	my($rdata, $client_socket_ref) = @_;
 
 	# write response data to the connected client
-    print '  Replying to client '.$CLIENT_IP.':'.$CLIENT_PORT;
-    my($client_size) = $CLIENT_SOCKET->send($rdata);
-	open(FILE,'>>server.log');
-	print FILE "\n".'////////// SENT TO CLIENT //////////'."\n";
-	print FILE $rdata;
-	close(FILE);
+    my $client_ip = $$client_socket_ref->peerhost();
+    my $client_port = $$client_socket_ref->peerport();
+    print '  Replying to client '.$client_ip.':'.$client_port;
+    my $client_size = $$client_socket_ref->send($rdata);
+	open(my $FILE, '>>server.log');
+	print $FILE "\n".'////////// SENT TO CLIENT //////////'."\n";
+	print $FILE $rdata;
+	close($FILE);
 	print ', sent data of length '.$client_size."\n";
 
     # notify client that response has been sent
-	$CLIENT_SOCKET->shutdown(SHUT_RDWR);
-	$CLIENT_SOCKET->close();	
+	$$client_socket_ref->shutdown(SHUT_RDWR);
+	$$client_socket_ref->close();	
 }
 
 
